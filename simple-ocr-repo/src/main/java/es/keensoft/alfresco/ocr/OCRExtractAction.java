b0c22acc10cefc949efe84253ee30adbfb5d1747
Angel Borroy <angel.borroy@keensoft.es>
2017-08-17 10:21:57 +0200

    OCR action executed in asynchronous mode
    To avoid OCR server overhead and time out responses to user requests

--- simple-ocr-repo/src/main/java/es/keensoft/alfresco/ocr/OCRExtractAction.java
+++ simple-ocr-repo/src/main/java/es/keensoft/alfresco/ocr/OCRExtractAction.java
@@ -5,11 +5,14 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.action.ParameterDefinitionImpl;
 import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
 import org.alfresco.repo.content.MimetypeMap;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
 import org.alfresco.repo.version.VersionModel;
@@ -43,9 +46,13 @@ public class OCRExtractAction extends ActionExecuterAbstractBase {
 	private TransactionService transactionService;
 	
 	private OCRTransformWorker ocrTransformWorker;
-	
+
+    private ThreadPoolExecutor threadPoolExecutor;
+
 	// Continue current operation in case of OCR error
 	private static final String PARAM_CONTINUE_ON_ERROR = "continue-on-error";
+	// Force asynchronous mode
+    private static final String PARAM_ASYNCHRONOUS = "asynchronous";
 	
 	public void init() {
 		super.init();
@@ -60,6 +67,12 @@ public class OCRExtractAction extends ActionExecuterAbstractBase {
 	                    DataTypeDefinition.BOOLEAN,
 	                    false,
                         getParamDisplayLabel(PARAM_CONTINUE_ON_ERROR)));
+        paramList.add(
+                new ParameterDefinitionImpl(
+                        PARAM_ASYNCHRONOUS,
+                        DataTypeDefinition.BOOLEAN,
+                        false,
+                        getParamDisplayLabel(PARAM_ASYNCHRONOUS)));
 	}
 
 	@Override
@@ -83,22 +96,34 @@ public class OCRExtractAction extends ActionExecuterAbstractBase {
             Boolean continueOnError = (Boolean) action.getParameterValue(PARAM_CONTINUE_ON_ERROR);
             if (continueOnError == null) continueOnError = true;
 
-            // # 5 Problem writing OCRed file
-            // As action.getExecuteAsychronously() returns always FALSE (it's an Alfresco issue):
-            // 1 - Try first with new Transaction
-            // 2 - In case of error, try then with the current Transaction
-            try {
-                executeInNewTransaction(actionedUponNodeRef, contentData);
-            } catch (Throwable throwableNewTransaction) {
-                logger.warn(actionedUponNodeRef + ": " + throwableNewTransaction.getMessage());
+            Boolean forceAsync = (Boolean) action.getParameterValue(PARAM_ASYNCHRONOUS);
+            if (forceAsync == null) forceAsync = false;
+
+            // Share action set asynchronous as mandatory due to variations in response time for OCR processes when server is busy 
+            if (forceAsync) {
+
+            	    Runnable runnable = new ExtractOCRTask(actionedUponNodeRef, contentData);
+            	    threadPoolExecutor.execute(runnable);
+
+            } else {
+
+                // # 5 Problem writing OCRed file
+                // As action.getExecuteAsychronously() returns always FALSE (it's an Alfresco issue):
+                // 1 - Try first with new Transaction
+                // 2 - In case of error, try then with the current Transaction
                 try {
-                    // Current transaction
-                    executeImplInternal(actionedUponNodeRef, contentData);
-                } catch (Throwable throwableCurrentTransaction) {
-                    if (continueOnError) {
-                        logger.warn(actionedUponNodeRef + ": " + throwableNewTransaction.getMessage());
-                    } else {
-                        throw throwableCurrentTransaction;
+                    executeInNewTransaction(actionedUponNodeRef, contentData);
+                } catch (Throwable throwableNewTransaction) {
+                    logger.warn(actionedUponNodeRef + ": " + throwableNewTransaction.getMessage());
+                    try {
+                        // Current transaction
+                        executeImplInternal(actionedUponNodeRef, contentData);
+                    } catch (Throwable throwableCurrentTransaction) {
+                        if (continueOnError) {
+                            logger.warn(actionedUponNodeRef + ": " + throwableNewTransaction.getMessage());
+                        } else {
+                            throw throwableCurrentTransaction;
+                        }
                     }
                 }
             }
@@ -106,7 +131,28 @@ public class OCRExtractAction extends ActionExecuterAbstractBase {
         }
 
 	}
-	
+
+    private class ExtractOCRTask implements Runnable {
+        
+        private NodeRef nodeToBeOCRd;
+        private ContentData contentData;
+         
+        private ExtractOCRTask(NodeRef nodeToBeOCRd, ContentData contentData) {
+            this.nodeToBeOCRd = nodeToBeOCRd;
+            this.contentData = contentData;
+        }
+        
+        @Override
+        public void run() {
+            AuthenticationUtil.runAsSystem(new RunAsWork<Void>() {
+                public Void doWork() throws Exception {
+                	    executeInNewTransaction(nodeToBeOCRd, contentData); 
+                    return null;
+                }
+            });
+        }
+    }
+    
     // Avoid ConcurrencyFailureException by using RetryingTransactionHelper
 	private void executeInNewTransaction(final NodeRef nodeRef, final ContentData contentData) {
 		
@@ -132,30 +178,30 @@ public class OCRExtractAction extends ActionExecuterAbstractBase {
 	    try {
 	        ocrTransformWorker.transform(reader, writer, null);
 	    } catch (Exception e) {
-	    	throw new RuntimeException(e);
+	    	    throw new RuntimeException(e);
 	    }
 	    
 	    // Set initial version if it's a new one
         versionService.ensureVersioningEnabled(actionedUponNodeRef, null);
 	    if (!versionService.isVersioned(actionedUponNodeRef)) {
-	    	Map<String, Serializable> versionProperties = new HashMap<String, Serializable>();
-	    	versionProperties.put(Version.PROP_DESCRIPTION, "OCRd");
-	    	versionProperties.put(VersionModel.PROP_VERSION_TYPE, VersionType.MINOR);
-	    	versionService.createVersion(actionedUponNodeRef, versionProperties);
+		    	Map<String, Serializable> versionProperties = new HashMap<String, Serializable>();
+		    	versionProperties.put(Version.PROP_DESCRIPTION, "OCRd");
+		    	versionProperties.put(VersionModel.PROP_VERSION_TYPE, VersionType.MINOR);
+		    	versionService.createVersion(actionedUponNodeRef, versionProperties);
 	    }
 	    
 	    ContentWriter writeOriginalContent = null;
 	    // Update original PDF file
 	    if (originalMimeType.equals(MimetypeMap.MIMETYPE_PDF)) {
-    	    writeOriginalContent = contentService.getWriter(actionedUponNodeRef, ContentModel.PROP_CONTENT, true);
+    	        writeOriginalContent = contentService.getWriter(actionedUponNodeRef, ContentModel.PROP_CONTENT, true);
 	    } else {
-	    	// Create new PDF file
-	    	String fileName = nodeService.getProperty(actionedUponNodeRef, ContentModel.PROP_NAME) + ".pdf";
-	    	Map<QName, Serializable> props = new HashMap<QName, Serializable>(1);
-	        props.put(ContentModel.PROP_NAME, fileName);
-	    	NodeRef pdfNodeRef = createNode(nodeService.getPrimaryParent(actionedUponNodeRef).getParentRef(), fileName, props);
-	    	writeOriginalContent = contentService.getWriter(pdfNodeRef, ContentModel.PROP_CONTENT, true);
-	    	writeOriginalContent.setMimetype(MimetypeMap.MIMETYPE_PDF);
+		    	// Create new PDF file
+		    	String fileName = nodeService.getProperty(actionedUponNodeRef, ContentModel.PROP_NAME) + ".pdf";
+		    	Map<QName, Serializable> props = new HashMap<QName, Serializable>(1);
+		        props.put(ContentModel.PROP_NAME, fileName);
+		    	NodeRef pdfNodeRef = createNode(nodeService.getPrimaryParent(actionedUponNodeRef).getParentRef(), fileName, props);
+		    	writeOriginalContent = contentService.getWriter(pdfNodeRef, ContentModel.PROP_CONTENT, true);
+		    	writeOriginalContent.setMimetype(MimetypeMap.MIMETYPE_PDF);
 	    }
 	    writeOriginalContent.putContent(writer.getReader());    	    
 		
@@ -164,7 +210,7 @@ public class OCRExtractAction extends ActionExecuterAbstractBase {
 		Map<String, Serializable> versionProperties = new HashMap<String, Serializable>();
 		versionProperties.put(Version.PROP_DESCRIPTION, "OCRd");
 		versionProperties.put(VersionModel.PROP_VERSION_TYPE, VersionType.MINOR);
-    	versionService.createVersion(actionedUponNodeRef, versionProperties);
+        versionService.createVersion(actionedUponNodeRef, versionProperties);
     	
 	    // Set OCRd aspect to avoid future re-OCR process
 	    Map<QName, Serializable> aspectProperties = new HashMap<QName, Serializable>();
@@ -202,5 +248,10 @@ public class OCRExtractAction extends ActionExecuterAbstractBase {
 	public void setTransactionService(TransactionService transactionService) {
 		this.transactionService = transactionService;
 	}
+
+    public void setThreadPoolExecutor(ThreadPoolExecutor threadPoolExecutor) {
+        this.threadPoolExecutor = threadPoolExecutor;
+    }
+
 
 }
